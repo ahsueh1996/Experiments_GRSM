aaf7fdaa9cc3e6fe1f39ecaa42a3fd83c1ee255c
hsuehku1 <hsuehku1@HPPworkstation.(none)>
2017-10-19 17:21:27 -0400

    optimization flag test

---
+++ high_level_2_architectural_features/mat_mul/README.md
@@ -0,0 +1,3 @@
+1) sh run.sh <machine> <N, dim of NxN aray>
+2) in make.sh change the optimization level by using the -o0 for no opt and -o for default
+3) all output gets stored in the correspondiing folders and changes to the params/script are over written
--- high_level_2_architectural_features/mat_mul/make.sh
+++ high_level_2_architectural_features/mat_mul/make.sh
@@ -1,6 +1,6 @@
 #$0 is the script name, $1 is the first arg, $n is the nth arg.
 echo "(1/2) writing assem to ass/$1.s ..."
-gcc src/$1.c -S -o $2/ass/$1.s
+gcc src/$1.c -S -o0 $2/ass/$1.s
 echo "(2/2) writing binary to bin/$1 ...."
-gcc src/$1.c -o $2/bin/$1
+gcc src/$1.c -o0 $2/bin/$1
 echo "(2/2) done"
