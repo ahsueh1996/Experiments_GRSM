9676410c56be86236e474a9c9dbe0274bdf8739b
Albert Hsueh <ahsueh1996@gmail.com>
2017-10-19 14:25:46 -0400

    rm files

--- high_level_2_architectural_features/if/src/if_range_ands.c
+++
@@ -1,9 +0,0 @@
-main() {
-	int x = rand() % 10
-	int y = rand() % 10
-	int a = 7
-	int b = 7
-	if (x > 0 && y > 0 && x < a && y < b) {
-	
-	}
-}
--- high_level_2_architectural_features/perf_test/src/test_int.c
+++
@@ -1,6 +0,0 @@
-main(){
-	int a = 1;
-	int b = 999999999999;
-	int c;
-	c = a + b;
-}
