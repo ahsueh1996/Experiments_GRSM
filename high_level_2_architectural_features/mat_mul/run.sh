26d6a551f62e2ae21b0722bfd602fbc487efc99d
hsuehku1 <hsuehku1@HPPworkstation.(none)>
2017-10-19 14:46:25 -0400

    omni N 256 512 64 for mat mul

--- high_level_2_architectural_features/mat_mul/run.sh
+++ high_level_2_architectural_features/mat_mul/run.sh
@@ -1,7 +1,10 @@
+# arg1 = omni | packet2a
+# arg2 = N
 # we need to run the shm_mat file first to create the shared matrix file
 sh make.sh shm_mat $1
 ./$1/bin/shm_mat $2
 # the key is 17 btw
+rm $1/perf/*
 declare -a tests=("base" "loop" "loop_ikj")
 for i in ${tests[@]}
 do
@@ -9,5 +12,5 @@ do
 	sh perf.sh $i $1 $2
 done
 # delete the shm
-#ipcrm -M 17
+ipcrm -M 17
 echo "done"
