a6841286876338c667f8f7856adee1c13d49d339
hsuehku1 <hsuehku1@HPPworkstation.(none)>
2017-10-26 15:05:55 -0400

    omni filtered asm res

--- high_level_2_architectural_features/mat_mul_java/run.sh
+++ high_level_2_architectural_features/mat_mul_java/run.sh
@@ -1,6 +1,6 @@
 # arg1 = omni | packet2a
 cd src
-mvn clean install
+#mvn clean install
 echo "=================================================="
 echo "running normal"
 #java -jar target/benchmarks.jar | tee ../$1/normal.txt
@@ -9,5 +9,5 @@ echo "running perfnorm"
 #java -jar target/benchmarks.jar -prof perfnorm | tee ../$1/perfnorm.txt
 echo "=================================================="
 echo "running assem"
-java -XX:+UnlockDiagnosticVMOptions -XX:+PrintAssembly -jar target/benchmarks.jar -prof perfasm | tee ../$1/perfasm.txt
+java -XX:+UnlockDiagnosticVMOptions -XX:CompileCommand=print,MyBenchmark -jar target/benchmarks.jar -prof perfasm | tee ../$1/perfasm.txt
 echo "done"
