297451f0b4f5975f0b0c341a68c73e061e18ab63
root <root@HPPworkstation.(none)>
2017-12-02 21:16:58 -0500

    add omni standalone results

--- jmh-spark/treeAggregate/batch-run.sh
+++ jmh-spark/treeAggregate/batch-run.sh
@@ -2,7 +2,7 @@ PD=/home/hsuehku1/Experiments_GRSM/jmh-spark/treeAggregate
 
 cd $PD
 
-sh run.sh aggressive standalone jmh_infused
+#sh run.sh aggressive standalone jmh_infused
 sh run.sh maxMargin_less_cmp standalone jmh_infused
 sh run.sh less_fcmp standalone jmh_infused
 sh run.sh int_label standalone jmh_infused
