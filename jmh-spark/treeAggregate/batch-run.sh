a7050e791d7f478cba554047acfff59e6f8b8d84
root <root@HPPworkstation.(none)>
2017-12-03 00:17:48 -0500

    update batch run and del odd files that got copied..?

--- jmh-spark/treeAggregate/batch-run.sh
+++ jmh-spark/treeAggregate/batch-run.sh
@@ -2,7 +2,17 @@ PD=/home/hsuehku1/Experiments_GRSM/jmh-spark/treeAggregate
 
 cd $PD
 
-#sh run.sh aggressive standalone jmh_infused
+sh run.sh baseline local
+sh run.sh baseline standalone
+sh run.sh large_pages local
+sh run.sh large_pages standalone
+sh run.sh aggressive local
+sh run.sh aggressive standalone jmh_infused
 sh run.sh maxMargin_less_cmp standalone jmh_infused
 sh run.sh less_fcmp standalone jmh_infused
 sh run.sh int_label standalone jmh_infused
+sh run.sh maxMargin_less_if standalone
+sh run.sh maxMargin_less_cmp local jmh_infused
+sh run.sh less_fcmp local jmh_infused
+sh run.sh int_label local jmh_infused
+sh run.sh maxMargin_less_if local
