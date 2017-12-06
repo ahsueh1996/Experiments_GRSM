d2e887ebd1e96c5296386228416cb6832abb567c
root <root@HPPworkstation.(none)>
2017-12-06 17:17:04 -0500

    current experiement updates

--- jmh-spark/treeAggregate/batch-run.sh
+++ jmh-spark/treeAggregate/batch-run.sh
@@ -2,8 +2,8 @@ PD=/home/hsuehku1/Experiments_GRSM/jmh-spark/treeAggregate
 
 cd $PD
 
-#sh run.sh baseline standalone
-#sh run.sh baseline local
+sh run.sh baseline standalone
+sh run.sh baseline local
 #sh run.sh large_pages local
 #sh run.sh large_pages standalone
 #sh run.sh aggressive local
@@ -16,8 +16,8 @@ cd $PD
 #sh run.sh less_fcmp local jmh_infused
 sh run.sh conglomerate local jmh_infused
 sh run.sh conglomerate standalone jmh_infused
-sh run.sh int_label local jmh_infused
-sh run.sh int_label standalone jmh_infused
-sh run.sh less_label local jmh_infused
-sh run.sh less_label standalone jmh_infused
+#sh run.sh int_label local jmh_infused
+#sh run.sh int_label standalone jmh_infused
+#sh run.sh less_label local jmh_infused
+#sh run.sh less_label standalone jmh_infused
 #sh run.sh maxMargin_less_if local
