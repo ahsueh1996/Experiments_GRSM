57544018979a8bc4cc289c8f61ea8b3dd685f9d1
Albert Hsueh <ahsueh1996@gmail.com>
2017-12-10 18:06:09 +0000

    add packet 350 results)

--- jmh-spark/treeAggregate/batch-run.sh
+++ jmh-spark/treeAggregate/batch-run.sh
@@ -3,15 +3,17 @@ pd=/home/hsuehku1/Experiments_GRSM/jmh-spark/treeAggregate
 td=/CMC/kmiecseb/spark/assembly/target/scala-2.11/jars
 
 cd $pd
-yes 'yes' | cp archive/hacks/conglomerate/* . -r
+#yes 'yes' | cp archive/hacks/conglomerate/* . -r
 
 cd $td
-sh $pd/hack.sh mllib/optimization spark-mllib_2.11-2.1.0.jar
+#sh $pd/hack.sh mllib/optimization spark-mllib_2.11-2.1.0.jar
 
 cd $pd
-sh run.sh conglomerate standalone no_jmh
-sh run.sh conglomerate local no_jmh
+#sh run.sh conglomerate standalone no_jmh
 
 cd $td
-yes 'yes' | sh $pd/unhack.sh mllib/optimization spark-mllib_2.11-2.1.0.jar
+yes 'yes' | sh $pd/unhack.sh spark-mllib_2.11-2.1.0.jar
+
+cd $pd
+sh run.sh baseline standalone no_jmh
 
