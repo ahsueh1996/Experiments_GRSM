0b38daf085cf3c5c2289fe8c625f8e66fe9d82f5
Albert Hsueh <ahsueh1996@gmail.com>
2017-12-11 15:55:36 +0000

    minor change to ordor run.sh

--- jmh-spark/order/run.sh
+++ jmh-spark/order/run.sh
@@ -1,7 +1,7 @@
 PROJ_DIR=/home/hsuehku1/Experiments_GRSM/jmh-spark/order
 cd $PROJ_DIR
-mkdir -p $PROJ_DIR/results/temp
-touch results/temp/experiment_log.txt
-echo Notes: $1 >> results/temp/experiment_log.txt
-java -jar target/benchmarks.jar -wm BULK | tee -a results/temp/experiment_log.txt
+mkdir -p /home/hibench-output/results
+touch results/experiment_log.txt
+echo Notes: $1 >> results/experiment_log.txt
+java -jar target/benchmarks.jar -wm BULK | tee -a results/experiment_log.txt
 
