c6a1111274ffbcc9f8822393f133341afd89663b
ahsueh1996 <ahsueh1996@gmail.com>
2017-12-13 18:08:48 -0500

    Update presentation_use_only.scala

--- jmh-spark/treeAggregate/archive/versions/1_main/presentation_use_only.scala
+++ jmh-spark/treeAggregate/archive/versions/1_main/presentation_use_only.scala
@@ -16,47 +16,29 @@ import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.storage.StorageLevel
 
 object Benchmarks {
-
         @State(Scope.Benchmark)
         class My_State extends {
-                var conf = new SparkConf()
+                val conf = new SparkConf()
                        		.setAppName("JMH prof: LogisticRegressionWithLBFGS")
-                          .setMaster("local[*]")
-                var sc = SparkContext.getOrCreate(conf)
+                          	.setMaster("local[*]")
+                val sc = SparkContext.getOrCreate(conf)
                 val inputPath = "hdfs://localhost:9000/HiBench/LR/Input"
-		            // Load training data in LIBSVM format.
-                var data: RDD[LabeledPoint] = sc.objectFile(inputPath).persist(StorageLevel.MEMORY_ONLY_SER)
-                // Split data into training (60%) and test (40%).
-                var splits = data.randomSplit(Array(0.6, 0.4), seed = 11L)
-                var training = splits(0).cache()
-                var test = splits(1)
+                val data: RDD[LabeledPoint] = sc.objectFile(inputPath).persist(StorageLevel.MEMORY_ONLY_SER)
+                val splits = data.randomSplit(Array(0.6, 0.4), seed = 11L)
+                val training = splits(0).cache()
+                val test = splits(1)
                 val v = Vectors
                         .dense(1.0)
                 var model = new LogisticRegressionModel(v,1.0)
-                @Setup(Level.Invocation)
-                def doSetup() {
-                        println(sc.getConf.getAll.deep.mkString("\n"))
-			                  println("set up")
-                }
-
-                @TearDown(Level.Invocation)
-                def doTearDown() {
-			                  println("tearing down...")
-                        sc.stop()
-			                  println("toredown")
-                }
-        }
-}
-
+		
+                @Setup(Level.Invocation) def doSetup() {println(sc.getConf.getAll.deep.mkString("\n"))}
+                @TearDown(Level.Invocation) def doTearDown() {sc.stop()}
+        } }
 class Benchmarks {
-        @Benchmark
-        @Fork(1)
-        @Warmup(iterations = 1, batchSize = 1)
-        @Measurement(iterations = 5, batchSize = 1)
+        @Benchmark @Fork(1) @Warmup(iterations = 1, batchSize = 1) @Measurement(iterations = 5, batchSize = 1)
         @BenchmarkMode(Array(Mode.SingleShotTime))
         def Multinomial_LR(s: Benchmarks.My_State) {
         	println("running...")        
 		          s.model = new LogisticRegressionWithLBFGS()
                   .setNumClasses(10).run(s.training)
-        }
-}
+        } }
